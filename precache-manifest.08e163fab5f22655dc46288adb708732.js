67b9a9540182edee9c047deccefde7b9339851b2
Devesh Pareek <thedeveshpareek@gmail.com>
2022-03-07 17:52:24 +0530

    Updates

---
+++ precache-manifest.08e163fab5f22655dc46288adb708732.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "6d497a06692217e017f3",
+    "url": "/static/css/main.cc4317e8.chunk.css"
+  },
+  {
+    "revision": "6d497a06692217e017f3",
+    "url": "/static/js/main.6d497a06.chunk.js"
+  },
+  {
+    "revision": "2e6b2fd0928cbac0fea7",
+    "url": "/static/js/1.2e6b2fd0.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "4d0f2715f91df6ed982682e8bf9e8c84",
+    "url": "/index.html"
+  }
+];
