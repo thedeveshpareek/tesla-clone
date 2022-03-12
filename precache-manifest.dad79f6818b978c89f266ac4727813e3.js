8cf0896a64119642fe78356b2a5491cb134ae010
Devesh pareek <thedeveshpareek@gmail.com>
2022-03-12 09:45:34 +0000

    Updates

---
+++ precache-manifest.dad79f6818b978c89f266ac4727813e3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "34843e0b7552b67c00e2",
+    "url": "/tesla-clone/static/css/main.3078a0d8.chunk.css"
+  },
+  {
+    "revision": "34843e0b7552b67c00e2",
+    "url": "/tesla-clone/static/js/main.34843e0b.chunk.js"
+  },
+  {
+    "revision": "40d075c0c2d6f4457630",
+    "url": "/tesla-clone/static/js/1.40d075c0.chunk.js"
+  },
+  {
+    "revision": "148239be0cad213eacd5",
+    "url": "/tesla-clone/static/js/runtime~main.148239be.js"
+  },
+  {
+    "revision": "d14a657189a3546dcfa218db97bbd3e9",
+    "url": "/tesla-clone/index.html"
+  }
+];
